0584440f2c5bb6c830b9b992d123370947250b06
Ahmad Elshowair <it.is.work@outlook.com>
2022-03-18 22:19:23 +0700

    add interests component

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
 import React from "react";
 import { About } from "./components/About";
 import { Info } from "./components/Info";
+import { Interests } from "./components/Interests";
 
 const App = ()=> {
   return (<div className="container">
       <div className="card">
           <Info />
           <About />
+          <Interests />
       </div>
   </div>);
 }
---
+++ src/components/Interests.js
@@ -0,0 +1,13 @@
+
+export const Interests = ()=>{
+    return(
+        <article className="card__interest">
+            <h3 className="card__interest-title">
+                Interests
+            </h3>
+            <p className="card__interest-text">
+                Tech, Travel, Languages
+            </p>
+        </article>
+    );
+};
