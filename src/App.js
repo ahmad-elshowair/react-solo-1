8985b8cd1f54659f248f07054f2ecbfa01e6c4f8
Ahmad Elshowair <it.is.work@outlook.com>
2022-03-18 22:14:40 +0700

    add about component

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import React from "react";
+import { About } from "./components/About";
 import { Info } from "./components/Info";
 
 const App = ()=> {
   return (<div className="container">
       <div className="card">
           <Info />
+          <About />
       </div>
   </div>);
 }
---
+++ src/components/About.js
@@ -0,0 +1,13 @@
+
+export const About = ()=>{
+    return(
+        <article className="card__about">
+            <h3 className="card__about-title">
+                About
+            </h3>
+            <p className="card__about-text">
+                I'm an inquisitive front-end developer who is always looking for improvement in everything that can make me better in all aspects. Experienced with 2+ years converting a variety of patterns of web designs into a responsive website from scratch or developing and fixing bugs.
+            </p>
+        </article>
+    );
+};
